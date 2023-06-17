14164035741abe3d73f389c0327cda17e3874f1c
jordannakamoto <jordannakamoto@gmail.com>
2023-06-17 08:25:21 -0700

    Cleaning up client errors and creating style for selected node

--- webapp/main.tsx
+++ webapp/main.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./src/App";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./css/customPopover.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const domNode = document.getElementById("root");
+if (domNode) {
+  const root = createRoot(domNode);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.log("domNode is null");
+}
--- webapp/src/components/FlowNodes/CourseNode.jsx
+++ webapp/src/components/FlowNodes/CourseNode.jsx
@@ -1,95 +1,92 @@
-import React, {useState} from "react";
-import {OverlayTrigger, Button, Popover} from "react-bootstrap";
-import {Handle, Position} from "reactflow";
-import '../../reactflow/floating_edges/style.css'
+import React, { useState } from "react";
+import { OverlayTrigger, Button, Popover } from "react-bootstrap";
+import { Handle, Position } from "reactflow";
+import "../../reactflow/floating_edges/style.css";
 
-const CourseNode = ({data, isConnectable}) => {
-    const [showPopover, setShowPopover] = useState(false);
+const CourseNode = ({ data, isConnectable }) => {
+  const [showPopover, setShowPopover] = useState(false);
 
-    document.addEventListener('keydown', (event) => {
-        if (event.key.toLowerCase() === 'escape' || event.code === '27') {
-            handleClosePopover();
-        }
-    });
+  document.addEventListener("keydown", (event) => {
+    if (event.key.toLowerCase() === "escape" || event.code === "27") {
+      handleClosePopover();
+    }
+  });
 
-    const handleNodeClick = () => {
-        setShowPopover(!showPopover);
-    };
+  const handleNodeClick = () => {
+    setShowPopover(!showPopover);
+  };
 
-    const handleClosePopover = () => {
-        setShowPopover(false);
-    };
+  const handleClosePopover = () => {
+    setShowPopover(false);
+  };
 
-    const popover = (
-        <Popover id="course-popover"
-                 className="custom-popover"
+  const popover = (
+    <Popover id="course-popover" className="custom-popover">
+      <Popover.Header className="custom-popover-header">
+        <div className="custom-popover-title">{data.label}</div>
+        <Button
+          variant="link"
+          className="custom-popover-close"
+          onClick={handleClosePopover}
         >
-            <Popover.Header className="custom-popover-header">
-                <div className="custom-popover-title">{data.label}</div>
-                <Button
-                    variant="link"
-                    className="custom-popover-close"
-                    onClick={handleClosePopover}
-                >
-                    <span className="custom-popover-close-icon">X</span>
-                </Button>
-            </Popover.Header>
-            <Popover.Body className="custom-popover-body">
-                <div>{data.desc}</div>
-            </Popover.Body>
-        </Popover>
-    );
+          <span className="custom-popover-close-icon">X</span>
+        </Button>
+      </Popover.Header>
+      <Popover.Body className="custom-popover-body">
+        <div>{data.desc}</div>
+      </Popover.Body>
+    </Popover>
+  );
 
-    return (
-        <>
-            <Handle
-                type="target"
-                position={Position.Left}
-                style={{
-                    background: "transparent"
-                }}
-                isConnectable={isConnectable}
-
-            />
-            <Handle
-                type="source"
-                position={Position.Right}
-                id="a"
-                onConnect={(params) => console.log("handle onConnect", params)}
-                isConnectable={isConnectable}
-            />
-            <div
-                className="node-wrapper"
-                onClick={handleNodeClick}
-                style={{
-                    background: "#fff",
-                    border: "1px solid #ccc",
-                    borderRadius: "8px",
-                    padding: "8px",
-                    cursor: "pointer",
-                    width: "140px",
-                }}
-            >
-                <div
-                    className="node-label"
-                    style={{
-                        marginBottom: "4px",
-                    }}
-                >
-                    {data.label}
-                </div>
-            </div>
-            <OverlayTrigger
-                trigger="click"
-                placement="auto"
-                show={showPopover}
-                onHide={handleClosePopover}
-                overlay={popover}
-            >
-                <div/>
-            </OverlayTrigger>
-        </>
-    );
+  return (
+    <>
+      <Handle
+        type="target"
+        position={Position.Left}
+        style={{
+          background: "transparent",
+        }}
+        isConnectable={isConnectable}
+      />
+      <Handle
+        type="source"
+        position={Position.Right}
+        id="a"
+        onConnect={(params) => console.log("handle onConnect", params)}
+        isConnectable={isConnectable}
+      />
+      <div
+        className="node-wrapper"
+        onClick={handleNodeClick}
+        style={{
+          background: "white",
+          border: "1px solid #ccc",
+          borderRadius: "8px",
+          padding: "8px",
+          cursor: "pointer",
+          width: "140px",
+        }}
+      >
+        <div
+          className="node-label"
+          style={{
+            marginBottom: "4px",
+          }}
+        >
+          {data.label}
+        </div>
+      </div>
+      <OverlayTrigger
+        trigger="click"
+        placement="auto"
+        show={showPopover}
+        onHide={handleClosePopover}
+        overlay={popover}
+      >
+        <div />
+      </OverlayTrigger>
+    </>
+  );
 };
 
 export default CourseNode;
