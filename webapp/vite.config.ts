bab5edec31d05eda5d90a0f55c1c3f4523b2b598
jordannakamoto <jordannakamoto@gmail.com>
2023-06-29 03:13:22 -0700

    removed package import

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { createProxyMiddleware } from "http-proxy-middleware";
 
 // https://vitejs.dev/config/
 export default defineConfig({
