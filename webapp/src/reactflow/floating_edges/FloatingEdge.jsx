7ebf98a036b8036d1729229a21004f3f9d3d77ec
Joel Thomas <joelthomas.e@gmail.com>
2023-07-22 16:23:40 -0700

    Added ability for user to delete edge

--- webapp/src/reactflow/floating_edges/FloatingEdge.jsx
+++ webapp/src/reactflow/floating_edges/FloatingEdge.jsx
@@ -1,14 +1,30 @@
-import { useCallback } from "react";
-import { useStore, getBezierPath, BaseEdge } from "reactflow";
+import React, { useCallback, useContext, useState } from "react";
+import {
+  useStore,
+  getBezierPath,
+  BaseEdge,
+  EdgeLabelRenderer,
+} from "reactflow";
 
 import { getEdgeParams } from "./utils.jsx";
+import { FlowNodesContext } from "../../common/Contexts";
 
-const FloatingEdge = ({ id, source, target, markerEnd, style }) => {
+const FloatingEdge = ({ id, source, target, markerEnd, style, selected }) => {
   const sourceNode = useStore(
     useCallback((store) => store.nodeInternals.get(source), [source])
   );
   const targetNode = useStore(
     useCallback((store) => store.nodeInternals.get(target), [target])
+  );
+
+  const { removeNodeEdge } = useContext(FlowNodesContext);
+
+  const onEdgeClose = useCallback(
+    (event, id) => {
+      event.stopPropagation();
+      removeNodeEdge(null, id);
+    },
+    [removeNodeEdge]
   );
 
   if (!sourceNode || !targetNode) {
@@ -20,7 +36,7 @@ const FloatingEdge = ({ id, source, target, markerEnd, style }) => {
     targetNode
   );
 
-  const [edgePath] = getBezierPath({
+  const [edgePath, labelX, labelY] = getBezierPath({
     sourceX: sx,
     sourceY: sy,
     sourcePosition: sourcePos,
@@ -31,14 +47,36 @@ const FloatingEdge = ({ id, source, target, markerEnd, style }) => {
 
   return (
     <>
-      <BaseEdge path={edgePath} markerEnd={markerEnd} interactionWidth={30} />
-      <path
+      <BaseEdge
         id={id}
-        className="react-flow__edge-path"
-        d={edgePath}
+        path={edgePath}
         markerEnd={markerEnd}
+        interactionWidth={30}
         style={style}
       />
+      <EdgeLabelRenderer>
+        <div
+          style={{
+            position: "absolute",
+            transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+            fontSize: 12,
+            pointerEvents: "all",
+          }}
+          className="nodrag nopan">
+          {selected && (
+            <button
+              className="edgebutton"
+              style={{
+                borderRadius: "25px",
+                color: "red",
+                backgroundColor: "white",
+              }}
+              onClick={(event) => onEdgeClose(event, id)}>
+              X
+            </button>
+          )}
+        </div>
+      </EdgeLabelRenderer>
     </>
   );
 };
