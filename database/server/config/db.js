082bb038dcceda4c059c7afba80f393e006dba81
jordannakamoto <jordannakamoto@gmail.com>
2023-08-07 06:14:51 -0700

    adjusted for MongoURI change

--- database/server/config/db.js
+++ database/server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
+  const conn = await mongoose.connect(process.env.MONGO_URI + "test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
