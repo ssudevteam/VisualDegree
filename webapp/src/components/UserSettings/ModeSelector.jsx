a533264f918b080a7ae7c237e7b818c878c0d1aa
jordannakamoto <jordannakamoto@gmail.com>
2023-08-11 17:20:49 -0700

    added keyboard shortcuts 1-3 to change mode

--- webapp/src/components/UserSettings/ModeSelector.jsx
+++ webapp/src/components/UserSettings/ModeSelector.jsx
@@ -1,13 +1,42 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState, useRef } from "react";
 import "../../../css/editor_toolbar.css";
 import { SelectorMode } from "../../common/Types";
 
 export default function ModeSelector({ setMode }) {
   const [currButtonId, setCurrButtonId] = useState("buttonMove");
+  const buttonIds = ["buttonMove", "buttonConnect", "buttonDescribe"];
+  const modes = [
+    SelectorMode.Move,
+    SelectorMode.Connect,
+    SelectorMode.Describe,
+  ];
+
+  const buttonRef = useRef(null);
 
   useEffect(() => {
     document.getElementById(currButtonId)?.classList.toggle("select", true);
-  }, []);
+
+    // Keyboard Event Handler
+    const handleKeyPress = (event) => {
+      const key = parseInt(event.key, 10);
+      if (key >= 1 && key <= buttonIds.length) {
+        const buttonId = buttonIds[key - 1];
+        const mode = modes[key - 1];
+        if (buttonRef.current && buttonId !== currButtonId) {
+          document
+            .getElementById(currButtonId)
+            ?.classList.toggle("select", false);
+          document.getElementById(buttonId)?.classList.toggle("select", true);
+          setMode(mode);
+          setCurrButtonId(buttonId);
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+
+    return () => window.removeEventListener("keydown", handleKeyPress);
+  }, [currButtonId, setMode, setCurrButtonId, buttonIds, modes]);
 
   const handleModeSelection = useCallback(
     (event, mode) => {
@@ -25,6 +54,7 @@ export default function ModeSelector({ setMode }) {
 
   return (
     <div
+      ref={buttonRef}
       id="modeSelector"
       style={{
         position: "absolute",
